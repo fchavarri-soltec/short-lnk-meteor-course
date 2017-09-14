15e0c10fe2c4ced9df2bb60375805f2a6c73aead
Felipe Chavarri <fchavarri@soltec.pe>
2017-09-14 11:11:34 -0500

    Initial commit

---
+++ imports/ui/LinksList.js
@@ -0,0 +1,54 @@
+import FlipMove from 'react-flip-move';
+import { Meteor } from 'meteor/meteor';
+import React from 'react';
+import { Session } from 'meteor/session';
+import { Tracker } from  'meteor/tracker';
+
+import { Links } from '../api/links';
+import LinksListItem from './LinksListItem';
+
+export default class LinksList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      links: []
+    };
+  }
+  componentDidMount() {
+    console.log('componentDidMount LinksList');
+    this.linksTracker = Tracker.autorun(() => {
+      Meteor.subscribe('links');
+      const links = Links.find({
+        visible: Session.get('showVisible')
+      }).fetch();
+      this.setState({ links });
+    });
+  }
+  componentWillUnmount() {
+    console.log('componentWillUnmount LinkList');
+    this.linksTracker.stop();
+  }
+  renderLinksListItems() {
+    if (this.state.links.length === 0) {
+      return (
+        <div className="item">
+          <p className="item__status-message">No se encontraron enlaces</p>
+        </div>
+      );
+    }
+
+    return this.state.links.map((link) => {
+      const shortUrl = Meteor.absoluteUrl(link._id);
+      return <LinksListItem key={link._id} shortUrl={shortUrl} {...link}/>;
+    });
+  }
+  render() {
+    return (
+      <div>
+        <FlipMove maintainContainerHeight={true}>
+          {this.renderLinksListItems()}
+        </FlipMove>
+      </div>
+    );
+  }
+}
